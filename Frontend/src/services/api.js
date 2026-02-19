471ab05c7b674b8c4617e843d6818dfb7b37e0ec
Tilak <adireddytilak@gmail.com>
2026-02-19 15:31:47 +0530

    Updated

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_URI}/api`
+  baseURL: `${import.meta.env.VITE_BASE_URI}`
 });
 
 export default API;
